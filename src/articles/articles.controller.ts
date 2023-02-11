5ff4c10da859ec4d3ede59423ed4cff3ac12bc22
roger-rocha <rogerrochabnusc@gmail.com>
2023-02-11 20:31:46 -0300

    initial commit

---
+++ src/articles/articles.controller.ts
@@ -0,0 +1,47 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException, ParseIntPipe, UseFilters } from '@nestjs/common';
+import { ArticlesService } from './articles.service';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { UpdateArticleDto } from './dto/update-article.dto';
+import { ApiTags, ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
+import { ArticleEntity } from './entities/article.entity';
+import { PrismaClientExceptionFilter } from 'src/prisma-client-exception/prisma-client-exception.filter';
+
+
+@Controller('articles')
+@ApiTags('articles')
+@UseFilters(PrismaClientExceptionFilter)
+export class ArticlesController {
+  constructor(private readonly articlesService: ArticlesService) {}
+
+  @Post()
+  @ApiCreatedResponse({ type: ArticleEntity })
+  create(@Body() createArticleDto: CreateArticleDto) {
+    return this.articlesService.create(createArticleDto);
+  }
+
+  @Get()
+  @ApiOkResponse({ type: ArticleEntity, isArray: true })
+  findAll() {
+    return this.articlesService.findAll();
+  }
+
+  @Get(':id')
+  @ApiOkResponse({ type: ArticleEntity })
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    const article = await this.articlesService.findOne(id);
+    if(!article) throw new NotFoundException(`Could not find article with id ${id}`);
+    return article;
+  }
+
+  @Patch(':id')
+  @ApiOkResponse({ type: ArticleEntity })
+  update(@Param('id', ParseIntPipe) id: number, @Body() updateArticleDto: UpdateArticleDto) {
+    return this.articlesService.update(id, updateArticleDto);
+  }
+
+  @Delete(':id')
+  @ApiOkResponse({ type: ArticleEntity })
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.articlesService.remove(id);
+  }
+}
